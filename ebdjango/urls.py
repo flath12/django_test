fcde1905a7138d4c7d384ffd61f7fb0f2e750440
Dan Flath <flath12@gmail.com>
2018-06-24 13:03:13 -0400

    First commit

--- ebdjango/urls.py
+++ ebdjango/urls.py
@@ -17,8 +17,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
+from rango import views
 
 urlpatterns = [
    url(r'^admin/', admin.site.urls),
    url(r'^', include('helloworld.urls')), 
+   url(r'^rango/', views.index, name='index'),
+   url(r'^rango/', include('rango.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
