8a17924f8add1653fee7a17366a9644a4cce0874
EC2 Default User <ec2-user@ip-172-31-8-129.us-west-2.compute.internal>
2018-06-28 00:39:42 +0000

    changed to postgresql

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -76,12 +76,24 @@ WSGI_APPLICATION = 'ebdjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_test',
+        'USER': 'admin',
+        'PASSWORD': 'admin1234',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
